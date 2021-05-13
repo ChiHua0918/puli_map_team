75579c53bce8d053334dccf885d1e7791b532e74
IanSChao <shouen.chao@gmail.com>
2021-05-13 21:35:59 +0800

    db connection

--- con_puli_db.php
+++ con_puli_db.php
@@ -3,11 +3,11 @@
     //  資料夾位置
     $db_server = "localhost";
     //  資料庫管理者帳號
-    $db_user = "root";
+    $db_user = "foodmap";
     //  資料庫管理者密碼
-    $db_password = "";
+    $db_password = "foodmap@puli#ncnu";
     //  資料庫名稱
-    $db_name = "wordpress";
+    $db_name = "testfoodmap";
     $link = mysqli_connect($db_server,$db_user,$db_password,$db_name); 
     if ($link === false) {
         die("連接失敗: ".mysqli_connect_error());
