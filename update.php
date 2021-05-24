b9b1ce1413eed3c2d9e5f884f6e91259cd39e96b
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2021-05-24 18:23:11 +0800

    update

--- update.php
+++ update.php
@@ -4,10 +4,20 @@
     include("login.php");
 
     @$RestaurantID = $_GET['RestaurantID'];
-//     @$RestaurantID = $_POST['RestaurantID'];
+// @$RestaurantID = $_POST['RestaurantID'];
+    @$RestaurantName = $_GET['RestaurantName'];
+    @$RestaurantTEL = $_GET['RestaurantTEL'];
+    @$RestaurantIntro = $_GET['RestaurantIntro'];
+    @$RestaurantTime = $_GET['RestaurantTime'];
+    @$RestaurantPhoto = $_POST['RestaurantPhoto'];
+    @$RestaurantComment = $_GET['RestaurantComment'];
+    @$RestaurantPrice = $_GET['RestaurantPrice'];
+    @$RestaurantAddress = $_GET['RestaurantAddress'];
+    @$RestaurantX = $_GET['RestaurantX'];
+    @$RestaurantY = $_GET['RestaurantY'];
 
-    @$key = $_GET['key'];
-    @$val = $_GET['val'];
+// @$key = $_GET['key'];
+// @$val = $_GET['val'];
 
     echo $_SESSION['userLogin'];
 
@@ -16,7 +26,11 @@ if(isset($_SESSION['userLogin']))
 {
         //更新資料庫資料語法
         $sql_update = "UPDATE puli_restaurant SET 
-        $key = '$val'
+        Restaurant_name = '$RestaurantName', Restaurant_TEL = '$RestaurantTEL', 
+        Restaurant_intro = '$RestaurantIntro', Restaurant_time = '$RestaurantTime',
+        Restaurant_photo = '$RestaurantPhoto', Restaurant_comment = '$RestaurantComment',
+        Restaurant_price = '$RestaurantPrice', Restaurant_address = '$RestaurantAddress',
+        Restaurant_x = '$RestaurantX', Restaurant_y = '$RestaurantY'
         WHERE Restaurant_ID = '$RestaurantID'";
 
         if(mysqli_query($link, $sql_update))
