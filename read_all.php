47df858858a7572d56de1a800ddbc80e1d1e5ec9
chen_chi_hua <s108213042@mail1.ncnu.edu.tw>
2021-06-13 02:40:37 +0800

    new

--- read_all.php
+++ read_all.php
@@ -4,7 +4,13 @@
     
     include("connect.php");
     // $sql 加入sql語法 從 user 的資料表中選擇所有欄位
-    $sql = "SELECT * FROM puli_restaurant";
+    $sql = "SELECT DISTINCT puli_restaurant.Restaurant_ID, puli_restaurant.Restaurant_name, puli_restaurant.Restaurant_TEL, 
+    puli_restaurant.Restaurant_intro, puli_restaurant.Restaurant_time, puli_restaurant.Restaurant_photo, 
+    puli_restaurant.Restaurant_comment, puli_restaurant.Restaurant_price, puli_restaurant.Restaurant_address, 
+    puli_restaurant.Restaurant_x, puli_restaurant.Restaurant_y, puli_restaurant.wordpress_link,puli_recommend.Category_name
+    FROM puli_restaurant
+    LEFT JOIN puli_rest_time on puli_restaurant.Restaurant_ID = puli_rest_time.Restaurant_ID 
+    LEFT JOIN puli_recommend on puli_recommend.Restaurant_ID = puli_restaurant.Restaurant_ID ";
     $result = $link->query($sql);
     $arr_data = [];
     if ($result -> num_rows > 0) {
@@ -22,7 +28,9 @@
             "RestaurantAddress" => $row["Restaurant_address"], 
             "RestaurantX" => $row["Restaurant_x"], 
             "RestaurantY" => $row["Restaurant_y"],
-            "wordpressLink" => $row['wordpress_link']);
+            "wordpressLink" => $row['wordpress_link'],
+            "CategoryName" => $row['Category_name']
+        );
             array_push($arr_data,$restaurant);
         }
 	echo json_encode($arr_data,JSON_NUMERIC_CHECK);
