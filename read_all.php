c351c590007973dfd2c86f93b72b0207330241f7
chen_chi_hua <s108213042@mail1.ncnu.edu.tw>
2021-07-12 23:20:19 +0800

    同一家餐廳顯示多個類別在同一行

--- read_all.php
+++ read_all.php
@@ -13,6 +13,8 @@
     LEFT JOIN puli_recommend on puli_recommend.Restaurant_ID = puli_restaurant.Restaurant_ID ";
     $result = $link->query($sql);
     $arr_data = [];
+    $addCategory = "";
+    $check = 0;
     if ($result -> num_rows > 0) {
         // 輸出數據
         while($row = $result->fetch_assoc()) {
@@ -28,9 +30,24 @@
             "RestaurantY" => $row["Restaurant_y"],
             "wordpressLink" => $row['wordpress_link'],
             "CategoryName" => $row['Category_name']
-        );
-            array_push($arr_data,$restaurant);
+            );
+            if ($check == $row["Restaurant_ID"]){
+                $addCategory .= $row['Category_name'];
+                // print_r(count($arr_data));
+                $arr_data[count($arr_data)-1]['CategoryName'] = $addCategory;
+            }
+            else{
+                array_push($arr_data,$restaurant);
+                $addCategory = $row['Category_name']; //紀錄目前餐廳ID類別
+            }
+            // 用來檢查餐廳ID是不是同一個  目的:讓一家餐廳的多類別顯示在同一行
+            $check = $row["Restaurant_ID"];
         }
+    for ($i = 0; $i < count($arr_data); $i++){
+        print_r($arr_data[$i]);
+        echo "<br/><br/>";
+    }
+
     echo json_encode($arr_data);
     // json_encode($arr_data,JSON_NUMERIC_CHECK);
     } else {
