f7a7b8b4af7b25933ab1a13121668b15007bb947
IanSChao <shouen.chao@gmail.com>
2021-07-19 13:36:40 +0800

    map

--- view/MapCreate.php
+++ view/MapCreate.php
@@ -33,7 +33,7 @@ if (!isset($_SESSION['userLogin'])) {
             z-index: 1;
             top: 20px;
             left: 3vw;
-            width: 35vw;
+            width: 55vw;
             height: 40vw;
         }
 
